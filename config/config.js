2465b1ab274f55eca2ee1b9ac390d458756793c4
Abhi Raj <ayushraj29102007@gmail.com>
2025-10-27 20:00:08 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // NineX - Secure Configuration (v11 - Final with New Credits)
 const PROXY_URL = '/api/proxy';
-const AIRTABLE_BASE_URL = 'https://api.airtable.com/v0/appgOPuIvNQ1eMYQw/tbls64uNeAgvXrZge';
+const AIRTABLE_BASE_URL = 'https://api.airtable.com/v0/appvf5cnySuHpWua4/tblCXb53fbuDTHt0E';
 
 const CONFIG = {
     API: { PROXY_URL, BASE_URL: AIRTABLE_BASE_URL },
